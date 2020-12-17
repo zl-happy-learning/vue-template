b52ab67f2059af39551649ab949beb91b41d2202
zl <791492434@qq.com>
2020-12-17 22:05:05 +0800

    ask.js
    ask

---
+++ ask.js
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    type: 'confirm',
+    name: 'private',
+    message: 'this registery is private?',
+  },
+
+  { type: 'input', author: 'author', message: 'author?' },
+  {
+    type: 'input',
+    author: 'description',
+    message: 'description?',
+  },
+  {
+    type: 'input',
+    author: 'license',
+    message: 'license?',
+  },
+];
